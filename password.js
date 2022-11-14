8016dfa2c1eb3ae99076bd2a073bc8d8deb821b2
kevin <kkevinlunaa95@gmail.com>
2022-11-14 20:08:59 +0100

    task 4 done pass

--- password.js
+++ password.js
@@ -1,14 +1,28 @@
 function validation (pass) {
-    if (pass.length >= 8 && /\d+.*\d/.test(pass) == true) {
+    if (pass.length >= 8 && /\d+.*\d/.test(pass) == true && /[A-Z]/.test(pass) == true) {
         return true;
-    } else if (pass.length < 8 && /\d+.*\d/.test(pass) == false){
+
+    } else if (pass.length < 8 && /\d+.*\d/.test(pass) == false && /[A-Z]/.test(pass) == false) {
+        return new Error("Password must be at least 8 characters, must contain at least 2 numbers and must contain at least 1 capital letter");
+
+    } else if (pass.length < 8 && /\d+.*\d/.test(pass) == false) {
         return new Error("Password must be at least 8 characters and must contain at least 2 numbers");
+
+    } else if (pass.length < 8 && /[A-Z]/.test(pass) == false) {
+        return new Error("Password must be at least 8 characters and must contain at least 1 capital letter");
+
+    } else if (/[A-Z]/.test(pass) == false && /\d+.*\d/.test(pass) == false) {
+        return new Error("Password must contain at least 2 numbers and must contain at least 1 capital letter");
+
     } else if (pass.length < 8) {
         return new Error("Password must be at least 8 characters");
+
     } else if (/\d+.*\d/.test(pass) == false) {
         return new Error("The password must contain at least 2 numbers");
-    };
-    console.log(pass.length)
+
+    } else if (/[A-Z]/.test(pass) == false) {
+        return new Error("Password must contain at least one capital letter");
+    }
 };
-console.log(validation("12345"))
+
 module.exports.validation = validation;
--- password.test.js
+++ password.test.js
@@ -2,33 +2,51 @@ const { validation } = require('./password.js');
 
 describe ('validator for the password field of a user registration form', () => {
 
-    it ('Password need +8 characters long', () =>{
-        expect(validation("12345678")).toBe(true);
+    it ('Password need +8 characters long', () => {
+        expect(validation("12345678")).not.toBe(true);
         expect(validation("qwerasdf")).not.toBe(true); //don't have 2 numbers, after is true but now is an error
-        expect(validation("as45/*rt651fg")).toBe(true);
+        expect(validation("As45/*rt651fg")).toBe(true);
         expect(validation("/*-+'()%")).not.toBe(true); //don't have 2 numbers, after is true but now is an error
-        expect(validation("12345")).toEqual(Error("Password must be at least 8 characters"));
-        expect(validation("12345")).toEqual(Error("Password must be at least 8 characters"));
+        expect(validation("1234Q")).toEqual(Error("Password must be at least 8 characters"));
+        expect(validation("12W45")).toEqual(Error("Password must be at least 8 characters"));
     })
     
-    it ('Password contain 2 numbers', () =>{
-        expect(validation("12345678")).toBe(true);
-        expect(validation("qw7rasd2")).toBe(true);
-        expect(validation("as45/*rt651fg")).toBe(true);
-        expect(validation("46qwerasdf")).toEqual(true);
-        expect(validation("qwe5asdk")).toEqual(Error("The password must contain at least 2 numbers"));
-        expect(validation("1/*-kldv")).toEqual(Error("The password must contain at least 2 numbers"));
-        expect(validation("/*-+*/--/")).toEqual(Error("The password must contain at least 2 numbers"));
-        expect(validation("eightdigits")).toEqual(Error("The password must contain at least 2 numbers"));
+    it ('Password contain 2 numbers', () => {
+        expect(validation("A12345678")).toBe(true);
+        expect(validation("qw7raSd2")).toBe(true);
+        expect(validation("as45/*rT651fg")).toBe(true);
+        expect(validation("46qweraTdf")).toEqual(true);
+        expect(validation("qwe5aSdK")).toEqual(Error("The password must contain at least 2 numbers"));
+        expect(validation("1/*-Kldv")).toEqual(Error("The password must contain at least 2 numbers"));
+        expect(validation("/*-+*/--W/")).toEqual(Error("The password must contain at least 2 numbers"));
+        expect(validation("eightDigits")).toEqual(Error("The password must contain at least 2 numbers"));
     })
+
+    it('Password contain one capital letter', () => {
+        expect(validation("Hello199")).toBe(true);
+        expect(validation("mR5l39lK")).toBe(true);
+        expect(validation("osp85-KL")).toBe(true);
+        expect(validation("4A4524663")).toBe(true);
+        expect(validation("/=sd09JS")).toBe(true);
+        expect(validation("6ello199")).toEqual(Error("Password must contain at least one capital letter"));
+        expect(validation("sdadfjh52")).toEqual(Error("Password must contain at least one capital letter"));
+        expect(validation("sdfdf89df")).toEqual(Error("Password must contain at least one capital letter"));
+        expect(validation("/*dfd54df")).toEqual(Error("Password must contain at least one capital letter"));
+        expect(validation("fghfvbn59")).toEqual(Error("Password must contain at least one capital letter"));
+    });
     
-    it('If pasword not have 8 characters and 2 numbers return multiple explains', () => {
-        expect(validation("qweras")).toEqual(Error("Password must be at least 8 characters and must contain at least 2 numbers"));
-        expect(validation("/*-+")).toEqual(Error("Password must be at least 8 characters and must contain at least 2 numbers"));
-        expect(validation("d5*")).toEqual(Error("Password must be at least 8 characters and must contain at least 2 numbers"));
-        expect(validation("6ekl*/")).toEqual(Error("Password must be at least 8 characters and must contain at least 2 numbers"));
-        expect(validation("d/*")).toEqual(Error("Password must be at least 8 characters and must contain at least 2 numbers"));
-        expect(validation("/red6d")).toEqual(Error("Password must be at least 8 characters and must contain at least 2 numbers"));
+    it('If pasword not have all requires return explicit error', () => {
+        expect(validation("qWeras")).toEqual(Error("Password must be at least 8 characters and must contain at least 2 numbers"));
+        expect(validation("/*A+")).toEqual(Error("Password must be at least 8 characters and must contain at least 2 numbers"));
+        expect(validation("D5*")).toEqual(Error("Password must be at least 8 characters and must contain at least 2 numbers"));
+        expect(validation("6eKl*/")).toEqual(Error("Password must be at least 8 characters and must contain at least 2 numbers"));
+        expect(validation("D/*")).toEqual(Error("Password must be at least 8 characters and must contain at least 2 numbers"));
+        expect(validation("/red66")).toEqual(Error("Password must be at least 8 characters and must contain at least 1 capital letter"));
+        expect(validation("8ed6d")).toEqual(Error("Password must be at least 8 characters and must contain at least 1 capital letter"));
+        expect(validation("sdgdfgghf")).toEqual(Error("Password must contain at least 2 numbers and must contain at least 1 capital letter"));
+        expect(validation("9dghnkms")).toEqual(Error("Password must contain at least 2 numbers and must contain at least 1 capital letter"));
+        expect(validation("/fgjd")).toEqual(Error("Password must be at least 8 characters, must contain at least 2 numbers and must contain at least 1 capital letter"));
+        expect(validation("9dfg*d")).toEqual(Error("Password must be at least 8 characters, must contain at least 2 numbers and must contain at least 1 capital letter"));
     });
 
 });
