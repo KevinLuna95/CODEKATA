800b603c84be465007821683b57b9eb6e951be44
kevin <kkevinlunaa95@gmail.com>
2022-11-14 08:18:07 +0100

    finish stringCalculator TDD

--- stringCalculator.js
+++ stringCalculator.js
@@ -3,9 +3,14 @@ try {
         if (chain == "" || chain == undefined) {
             return 0;
         };
-        var bandComma = chain.includes(",");
+        var bandComma = chain.includes("," || "\n");
         if(bandComma == true) {
-            let numbers = chain.split(/,/);
+            var bandSep = chain.search(/,$|\n$/gm);
+            if (bandSep != -1 ){
+                return "Value contains separator at end of text and is not allowed or the declaration is invalid"
+            }
+
+            let numbers = chain.split(/,|\n|\||;|sep/);
             var a = 0, b = 0;
             for (let i = 0; i < numbers.length; i++) {
                 a = parseInt(numbers[i]);
@@ -20,7 +25,7 @@ try {
         return parseInt(chain);
     };
 
-    console.log(calculator("2,8,5,6,8"))
+    console.log(calculator("31,40\n"))
 
 } catch (error) {
     console.log(error);
--- stringCalculator.test.js
+++ stringCalculator.test.js
@@ -33,5 +33,33 @@ describe('String test calculator', () => {
         expect(calculator("161,14")).toEqual(175);
         expect(calculator("0,0")).toEqual(0);
     });
+    
+    it('add all the values ​​unknown the number of numbers', () =>{
+        expect(calculator("1,1,3,6")).toEqual(11);
+        expect(calculator("1,6,66")).toEqual(73);
+        expect(calculator("31,61,6,8,10,33")).toEqual(149);
+        expect(calculator("0,0,0,0,0")).toEqual(0);
+    });
+
+    it('use "\n" as a separator', () =>{
+        expect(calculator("1,2\n3")).toEqual(6);
+        expect(calculator("2,8\n4")).toEqual(14);
+        expect(calculator("9,21\n36")).toEqual(66);
+        expect(calculator("50,21,5\n1")).toEqual(77);
+        expect(calculator("5,3\n7\n2")).toEqual(17);
+        expect(calculator("7,6\n9\n1\n4")).toEqual(27);
+        expect(calculator("9,1,6,8\n5\n8\n3\n6\n1")).toEqual(47);
+    });
+
+    it('use other separators', () =>{
+        expect(calculator("1,2|3")).toEqual(6);
+        expect(calculator("2,8sep4")).toEqual(14);
+        expect(calculator("9,21;36")).toEqual(66);
+        expect(calculator("50,21sep5;1")).toEqual(77);
+        expect(calculator("5,3|7\n2")).toEqual(17);
+        expect(calculator("7,6;9sep1;4")).toEqual(27);
+        expect(calculator("9,1;6|8\n5sep8,3sep6;1")).toEqual(47);
+    });
+
 
 });
