cc4c2e3a20b31c374fb4a894e2b8b544363685d9
kevin <kkevinlunaa95@gmail.com>
2022-11-16 08:38:48 +0100

    task 1 done searchCity

--- searchCity.js
+++ searchCity.js
@@ -1,8 +1,10 @@
 const CityNames = ["Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver", "Amsterdam", 
 "Vienna", "Sydney", "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul"];
 
-function searchCity (){
-
+function searchCity (text){
+    if (text.length < 2){
+        return Error("Write 2 or more characrters for find the city");
+    }
 }
 
 module.exports.searchCity = searchCity;
--- searchCity.test.js
+++ searchCity.test.js
@@ -2,10 +2,12 @@ const { searchCity } = require("./searchCity");
 
 describe('searchCity is a function that allows to find the name of the city', ()=> {
     it('If the search text is fewer than 2 characters, then should return no results.',()=> {
-        expect(searchCity("a")).toBe(/write 2 or more characrters for find the city/);
-        expect(searchCity("l")).toBe(/write 2 or more characrters for find the city/);
-        expect(searchCity("e")).toBe(/write 2 or more characrters for find the city/);
-        expect(searchCity("b")).toBe(/write 2 or more characrters for find the city/);
-        expect(searchCity("z")).toBe(/write 2 or more characrters for find the city/);
+        expect(searchCity("a")).toEqual(Error("Write 2 or more characrters for find the city"));
+        expect(searchCity("l")).toEqual(Error("Write 2 or more characrters for find the city"));
+        expect(searchCity("e")).toEqual(Error("Write 2 or more characrters for find the city"));
+        expect(searchCity("b")).toEqual(Error("Write 2 or more characrters for find the city"));
+        expect(searchCity("z")).toEqual(Error("Write 2 or more characrters for find the city"));
+        expect(searchCity("as")).not.toEqual(Error("Write 2 or more characrters for find the city"));
+        expect(searchCity("as")).not.toEqual(Error("Write 2 or more characrters for find the city"));
     });
 });
